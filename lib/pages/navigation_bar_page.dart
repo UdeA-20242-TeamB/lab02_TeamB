fbce832969134d8a289c48d7ecd5ac38cf07b301
Camilo Alvarez Muñoz <camilo.alvarezm@udea.edu.co>
2024-09-23 16:57:19 -0500

    Created a page dedicated to the navigation method

---
+++ lib/pages/navigation_bar_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NavigationBarPage extends StatefulWidget {
+  const NavigationBarPage({super.key});
+
+  @override
+  State<NavigationBarPage> createState() => _NavigationBarPageState();
+}
+
+class _NavigationBarPageState extends State<NavigationBarPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
