ec25ce76e84dd033805ab5b28258718a59a900cb
pablomunoz24 <pablo.munoz1@udea.edu.co>
2024-09-28 18:04:13 -0500

    Finalizado Area del cuadrado

--- lib/pages/area_square.dart
+++ lib/pages/area_square.dart
@@ -9,12 +9,52 @@ class AreaSquare extends StatefulWidget {
 }
 
 class _AreaSquareState extends State<AreaSquare> {
+  final _ladocuadrado = TextEditingController();
+  double _areacuadrado = 0.0;
+
+  void _CalcularAreacuradr() {
+    setState(() {
+      double input_double = double.tryParse(_ladocuadrado.text) ?? 0.0;
+      _areacuadrado = input_double * input_double;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Area Cuadrado"),
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+          child: Column(
+            children: [
+              const Image(
+                image: AssetImage('assets/images/area_cuadrado.jpg'),
+              ),
+              const SizedBox(
+                height: 16.0,
+              ),
+              TextFormField(
+                controller: _ladocuadrado,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  labelText: "Ingrese el lado del caudrado",
+                ),
+                keyboardType: TextInputType.number,
+              ),
+              const SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: _CalcularAreacuradr,
+                child: const Text('Calcular Area del cuadrado'),
+              ),
+              const SizedBox(height: 16.0),
+              Text('El area del cuadrado es: $_areacuadrado')
+            ],
+          ),
+        ),
       ),
     );
   }
 }
+//Pablo Francisco Muñoz
